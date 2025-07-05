f8b50887bb53e7be1d912234a1155634ebe41458
Rahul Dey <rahuldeybl156@gmail.com>
2025-07-05 22:22:10 +0600

    Update HandControlledFrequency.ino
    Updated with LED and Buzzer

--- HandControlledFrequency.ino
+++ HandControlledFrequency.ino
@@ -1,5 +1,5 @@
-int buzzPin = 7;
-int length, freq;
+int buzzPin = 6, ledPin = 9;
+int length, signal;
 
 void setup() {
   Serial.begin(9600);
@@ -12,26 +12,14 @@ void loop() {
     length = (Serial.readStringUntil('\r')).toInt();
   }
 
-  if (length >= 30 && length <= 240) {
-    buzz(length);
-    digitalWrite(13, HIGH);
+  signal = map(length, 30, 300, 0, 255);
+
+  if (length >= 30 && length <= 300) {
+    analogWrite(ledPin, signal);
+    analogWrite(buzzPin, signal);
   }
   else {
-    digitalWrite(buzzPin, LOW);
-    digitalWrite(13, LOW);
-  }
-  // delay(0.25);
-}
-
-void buzz(int length) {
-  freq = map(length, 30, 240, 20, 2000);
-  float period = 1000000.0 / freq;
-  unsigned long duration = 250000;  // 50ms
-
-  for (unsigned long t = 0; t < duration; t += period) {
-    digitalWrite(buzzPin, HIGH);
-    delayMicroseconds(period / 2);
-    digitalWrite(buzzPin, LOW);
-    delayMicroseconds(period / 2);
+    analogWrite(ledPin, 0);
+    analogWrite(buzzPin, 0);
   }
 }
